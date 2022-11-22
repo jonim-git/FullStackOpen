f9a3414656b030e3a95e0a85ecd95ba30fb1e97c
jonim-git <jonimaki1@gmail.com>
2022-11-22 21:54:25 +0200

    1.1-1.14 tehty

---
+++ part1/anekdootit/src/App.js
@@ -0,0 +1,59 @@
+
+import { useState } from 'react'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Anecdotes = ({ anecdote, votes, winnerVote }) => {
+
+  return (
+    <div>
+      <h1>Random anecdote:</h1>
+      {anecdote}
+      <p> votes: {votes} </p>
+      <h2>Winner anecdote</h2>
+      {winnerVote}
+    </div>
+  )
+}
+
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [array, setArray] = useState(new Uint8Array(7))
+
+  const winnerVote = array.indexOf(Math.max(...array))
+  const random = () => Math.floor(Math.random() * (anecdotes.length - 1 + 1)) + 0;
+  const nextAnecdote = () => setSelected(random)
+
+  const vote = () => {
+    const copy = [...array]
+    copy[selected] += 1
+    setArray(copy)
+  }
+
+  return (
+    <div>
+      <Anecdotes anecdote={anecdotes[selected]} votes={array[selected]} winnerVote={anecdotes[winnerVote]} />
+      <Button handleClick={nextAnecdote} text={"Next random anecdote"} />
+      <Button handleClick={vote} text={"Vote for this anecdote"} />
+    </div>
+  )
+}
+
+export default App
--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -10,9 +10,11 @@ const Header = (props) => {
 
 const StatisticHeader = (props) => {
   return (
-    <>
-      <h2>statistics</h2>
-    </>
+    <thead>
+      <tr>
+        <th>statistics</th>
+      </tr>
+    </thead>
   )
 }
 
@@ -24,31 +26,109 @@ const Button = (props) => {
   )
 }
 
+const StatisticLine = (props) => {
+  return (
+    <>
+      <tr>
+        <td>
+          {props.text}
+        </td>
+        <td>
+          {props.value}
+        </td>
+      </tr>
+    </>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, total, average }) => {
+  const fbAvg = average / total
+  const positivePercentage = good / total * 100
+
+  if (total === 0) {
+    return (
+      <table>
+        <StatisticHeader />
+        <tbody>
+          <tr>
+            <td>
+              No feedback given yet
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+
+  return (
+
+    <table>
+      <StatisticHeader />
+      <tbody>
+        <StatisticLine text='good (+1)' value={good} />
+        <StatisticLine text='neutral (+0)' value={neutral} />
+        <StatisticLine text='bad (-1)' value={bad} />
+        <StatisticLine text='total feedback' value={total} />
+        <StatisticLine text='feedback average' value={fbAvg} />
+        <StatisticLine text='positive feedback percentage' value={positivePercentage + ' %'} />
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+
+  const handleGood = (e) => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setAverage(average + 1)
+    e.preventDefault()
+  }
+
+  const handleNeutral = (e) => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+    setAverage(average + 0)
+    e.preventDefault()
+  }
+
+  const handleBad = (e) => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setAverage(average - 1)
+    e.preventDefault()
+  }
+  //EI TOIMI 
+  console.log(average, 'average')
+  console.log(total, 'total')
 
   return (
     <div>
       <Header />
       <Button
-        handleClick={() => setGood(good + 1)}
+        handleClick={handleGood}
         text='good'
       />
       <Button
-        handleClick={() => setNeutral(neutral + 1)}
+        handleClick={handleNeutral}
         text='neutral'
-      />     
+      />
       <Button
-        handleClick={() => setBad(bad + 1)}
+        handleClick={handleBad}
         text='bad'
-      />   
-      <StatisticHeader/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      />
+      <Statistics
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        total={total}
+        average={average} />
     </div>
   )
 }
